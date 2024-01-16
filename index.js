5deacf983752afd4a97efef9791b74201d65a3d3
narayan pravin deshpande <102522755+narayandeshpande@users.noreply.github.com>
2024-01-16 14:28:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+document.querySelector('.cros').style.display='none';
+document.querySelector('.hamb').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('gosidebar');
+
+})
+
